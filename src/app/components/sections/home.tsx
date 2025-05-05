cc17c778eebefa3a7ebf467e459369b1599c31a3
dinolan-naidoo-1 <dinolannaidoo9@gmail.com>
2025-05-05 19:41:46 +0200

    fix: animated text update

--- src/app/components/sections/home.tsx
+++ src/app/components/sections/home.tsx
@@ -14,91 +14,130 @@ const HomeSection: React.FC<SectionProps> = ({ id }) => {
                     <div className="text-content">
                         <h1 className="main-heading">ELEVATING BRANDS WITH</h1>
 
-                        {/* Animated Text */}
                         <div className="animated-text-container">
                             <style
                                 dangerouslySetInnerHTML={{
                                     __html: `
-                    @keyframes bounceIn {
-                      0% {
+                @keyframes bounceIn {
+                    0% {
                         opacity: 0;
                         transform: scale(0);
-                      }
-                      20% {
+                    }
+                    20% {
                         opacity: 1;
-                      }
-                      40% {
+                    }
+                    40% {
                         transform: scale(1.15)
-                      }
-                      70% {
+                    }
+                    70% {
                         transform: scale(.9)
-                      }
-                      100% {
+                    }
+                    100% {
                         transform: scale(1);
                         opacity: 1;
-                      }
                     }
+                }
 
-                    .animated-letter {
-                      display: inline-block;
-                      animation: bounceIn 6s forwards;
-                      animation-timing-function: ease-out;
-                      opacity: 0;
-                      animation-iteration-count: infinite;
-                      animation-direction: alternate;
-                      animation-duration: 6s;
+                .animated-heading-wrapper {
+                    display: flex;
+                    flex-wrap: nowrap;
+                }
+
+                .text-group {
+                    display: inline-block;
+                    white-space: nowrap;
+                    margin-right: 0.9rem;
+                }
+
+                .animated-letter {
+                    display: inline-block;
+                    animation: bounceIn 6s forwards;
+                    animation-timing-function: ease-out;
+                    opacity: 0;
+                    animation-iteration-count: infinite;
+                    animation-direction: alternate;
+                    animation-duration: 6s;
+                }
+                
+                /* Exact same animation timing as before, but for the global index */
+                /* 3 */ .letter-0 { animation-delay: 0.1s; }
+                /* D */ .letter-1 { animation-delay: 0.2s; }
+                /* M */ .letter-2 { animation-delay: 0.3s; margin-left: 12px }
+                /* O */ .letter-3 { animation-delay: 0.4s; }
+                /* D */ .letter-4 { animation-delay: 0.5s; }
+                /* E */ .letter-5 { animation-delay: 0.6s; }
+                /* L */ .letter-6 { animation-delay: 0.7s; }
+                /* S */ .letter-7 { animation-delay: 0.8s; }
+                /* ; */ .letter-8 { animation-delay: 0.9s; }
+                /* A */ .letter-9 { animation-delay: 1.0s;}
+                /* N */ .letter-10 { animation-delay: 1.1s; }
+                /* I */ .letter-11 { animation-delay: 1.2s; }
+                /* M */ .letter-12 { animation-delay: 1.3s; }
+                /* A */ .letter-13 { animation-delay: 1.4s; }
+                /* T */ .letter-14 { animation-delay: 1.5s; }
+                /* I */ .letter-15 { animation-delay: 1.6s; }
+                /* O */ .letter-16 { animation-delay: 1.7s; }
+                /* N */ .letter-17 { animation-delay: 1.8s; }
+                /* S */ .letter-18 { animation-delay: 1.9s; }
+                /* ; */ .letter-19 { animation-delay: 2.0s; }
+                /* A */ .letter-20 { animation-delay: 2.1s; }
+                /* U */ .letter-21 { animation-delay: 2.2s; }
+                /* G */ .letter-22 { animation-delay: 2.3s; }
+                /* M */ .letter-23 { animation-delay: 2.4s; }
+                /* E */ .letter-24 { animation-delay: 2.5s; }
+                /* N */ .letter-25 { animation-delay: 2.6s; }
+                /* T */ .letter-26 { animation-delay: 2.7s; }
+                /* E */ .letter-27 { animation-delay: 2.8s; }
+                /* D */ .letter-28 { animation-delay: 2.9s; }
+                /* R */ .letter-29 { animation-delay: 3.0s; }
+                /* E */ .letter-30 { animation-delay: 3.1s; margin-left: 12px; }
+                /* A */ .letter-31 { animation-delay: 3.2s; }
+                /* L */ .letter-32 { animation-delay: 3.3s; }
+                /* I */ .letter-33 { animation-delay: 3.4s; }
+                /* T */ .letter-34 { animation-delay: 3.5s; }
+                /* Y */ .letter-35 { animation-delay: 3.6s; }
+                       .letter-36 { animation-delay: 3.6s; }
+                       .letter-37 { animation-delay: 3.6s; }
+                       .letter-38 { animation-delay: 3.6s; }
+
+                @media (max-width: 768px) {
+                    .animated-heading-wrapper {
+                        flex-wrap: wrap;
                     }
                     
-                    /* Stagger animation for each letter */
-                    /* 3 */ .animated-letter:nth-child(1) { animation-delay: 0.1s; }
-                    /* D */ .animated-letter:nth-child(2) { animation-delay: 0.2s; }
-                    /* M */ .animated-letter:nth-child(3) { animation-delay: 0.3s; margin-left: 15px }
-                    /* O */ .animated-letter:nth-child(4) { animation-delay: 0.4s; }
-                    /* D */ .animated-letter:nth-child(5) { animation-delay: 0.5s; }
-                    /* E */ .animated-letter:nth-child(6) { animation-delay: 0.6s; }
-                    /* L */ .animated-letter:nth-child(7) { animation-delay: 0.7s; }
-                    /* S */ .animated-letter:nth-child(8) { animation-delay: 0.8s; }
-                    /* ; */ .animated-letter:nth-child(9) { animation-delay: 0.9s; }
-                    /* A */ .animated-letter:nth-child(10) { animation-delay: 1.0s; margin-left: 15px; }
-                    /* N */ .animated-letter:nth-child(11) { animation-delay: 1.1s; margin-left: 15px;}
-                    /* I */ .animated-letter:nth-child(12) { animation-delay: 1.2s; }
-                    /* M */ .animated-letter:nth-child(13) { animation-delay: 1.3s; }
-                    /* A */ .animated-letter:nth-child(14) { animation-delay: 1.4s; }
-                    /* T */ .animated-letter:nth-child(15) { animation-delay: 1.5s; }
-                    /* I */ .animated-letter:nth-child(16) { animation-delay: 1.6s; }
-                    /* O */ .animated-letter:nth-child(17) { animation-delay: 1.7s; }
-                    /* N */ .animated-letter:nth-child(18) { animation-delay: 1.8s; }
-                    /* S */ .animated-letter:nth-child(19) { animation-delay: 1.9s; }
-                    /* ; */ .animated-letter:nth-child(20) { animation-delay: 2.0s; }
-                    /* A */ .animated-letter:nth-child(21) { animation-delay: 2.1s; margin-left: 15px; }
-                    /* U */ .animated-letter:nth-child(22) { animation-delay: 2.2s; margin-left: 15px;}
-                    /* G */ .animated-letter:nth-child(23) { animation-delay: 2.3s; }
-                    /* M */ .animated-letter:nth-child(24) { animation-delay: 2.4s; }
-                    /* E */ .animated-letter:nth-child(25) { animation-delay: 2.5s; }
-                    /* N */ .animated-letter:nth-child(26) { animation-delay: 2.6s; }
-                    /* T */ .animated-letter:nth-child(27) { animation-delay: 2.7s; }
-                    /* E */ .animated-letter:nth-child(28) { animation-delay: 2.8s; }
-                    /* D */ .animated-letter:nth-child(29) { animation-delay: 2.9s; }
-                    /* R */ .animated-letter:nth-child(30) { animation-delay: 3.0s;  }
-                    /* E */ .animated-letter:nth-child(31) { animation-delay: 3.1s; margin-left: 15px; }
-                    /* A */ .animated-letter:nth-child(32) { animation-delay: 3.2s; }
-                    /* L */ .animated-letter:nth-child(33) { animation-delay: 3.3s; }
-                    /* I */ .animated-letter:nth-child(34) { animation-delay: 3.4s; }
-                    /* T */ .animated-letter:nth-child(35) { animation-delay: 3.5s; }
-                    /* Y */ .animated-letter:nth-child(36) { animation-delay: 3.6s; }
-                     .animated-letter:nth-child(37) { animation-delay: 3.6s; }
-                     .animated-letter:nth-child(38) { animation-delay: 3.6s; }
-                     .animated-letter:nth-child(39) { animation-delay: 3.6s; }
-                  `,
+                    .text-group {
+                        width: 100%;
+                        margin-bottom: 0.5rem;
+                    }
+                }
+            `,
                                 }}
                             />
 
                             <h1 className="animated-heading">
-                                {"3D MODELS;ANIMATIONS;AUGMENTED REALITY".split("").map((char, index) => (
-                                    <span key={index} className="animated-letter">
-                    {char}
-                  </span>
-                                ))}
+                                <div className="animated-heading-wrapper">
+            <span className="text-group">
+                {"3D MODELS;".split("").map((char, index) => (
+                    <span key={index} className={`animated-letter letter-${index}`}>
+                        {char}
+                    </span>
+                ))}
+            </span>
+                                    <span className="text-group">
+                {"ANIMATIONS;".split("").map((char, index) => (
+                    <span key={index} className={`animated-letter letter-${index + 9}`}>
+                        {char}
+                    </span>
+                ))}
+            </span>
+                                    <span className="text-group">
+                {"AUGMENTED REALITY".split("").map((char, index) => (
+                    <span key={index} className={`animated-letter letter-${index + 20}`}>
+                        {char}
+                    </span>
+                ))}
+            </span>
+                                </div>
                             </h1>
                         </div>
 
@@ -218,7 +257,7 @@ const HomeSection: React.FC<SectionProps> = ({ id }) => {
               .animated-heading {
                 padding-top: 1rem;
                 text-align: left;
-                font-size: 1.7rem;
+                font-size: 1.8rem;
                 color: #9f3c5e;
                 font-family: var(--font-quicksand);
                 transform-origin: center bottom;
@@ -349,7 +388,7 @@ const HomeSection: React.FC<SectionProps> = ({ id }) => {
                 }
 
                 .animated-heading {
-                  font-size: 1.5rem;
+                  font-size: 1.7rem;
                 }
 
                 .description-text {
@@ -380,7 +419,7 @@ const HomeSection: React.FC<SectionProps> = ({ id }) => {
                 }
 
                 .animated-heading {
-                  font-size: 1.2rem;
+                  font-size: 1.4rem;
                 }
 
                 .service-text {
